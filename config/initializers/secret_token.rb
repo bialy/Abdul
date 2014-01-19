22159c13758806cfdc22074a4c408a94cdc1abc8
Bialy <bialy@bialy.(none)>
2014-01-19 12:45:31 +0100

    new app Abdul

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Abdul::Application.config.secret_token = '795d68ba71c921260c56445090d141eaaa45fb6c28dd19a1617259e749668bc67fec8592662a06d3d0415f6dcec83a7701ec5ea1e784e9a9fdc2d7e038988208'
